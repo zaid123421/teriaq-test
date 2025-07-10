34abd1a8109d0683829a0df89c254216c25801d0
Zaid Alshamaa <zshamaa20@gmail.com>
2025-07-10 22:24:26 +0300

    style: make Footer component fully responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,22 +7,22 @@ import { FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return(
-    <footer className='flex flex-col items-center justify-center py-8 bg-[#251f1d]'>
-      <img alt='logo' src={Logo} />
+    <footer className='text-base md:text-lg lg:text-xl flex flex-col items-center justify-center px-2 py-4 md:py-8 bg-[#251f1d]'>
+      <img className='w-[100px] h-[100px]' alt='logo' src={Logo} />
       <nav className='w-full'>
-        <ul className='text-white mt-5 flex flex-row-reverse justify-center'>
-          <Link toPlace="/" label="الرئيسية" />
-          <Link toPlace="#most-popular" label="الأصناف الأكثر طلباً" />
-          <Link toPlace="#food-list" label="قائمة الطعام" />
-          <Link toPlace="#who-us" label="من نحن" />
-          <Link label="المقالات" className={`cursor-pointer`} />
-          <Link toPlace="#contact-us" label="تواصل معنا" />
+        <ul className='text-white mt-5 flex flex-wrap items-center flex-row-reverse justify-center'>
+          <Link className="ml-4 mb-2" toPlace="/" label="الرئيسية" />
+          <Link className="ml-4 mb-2" toPlace="#most-popular" label="الأصناف الأكثر طلباً" />
+          <Link className="ml-4 mb-2" toPlace="#food-list" label="قائمة الطعام" />
+          <Link className="ml-4 mb-2" toPlace="#who-us" label="من نحن" />
+          <Link className="ml-4 mb-2" label="المقالات" />
+          <Link className="ml-4 mb-2" toPlace="#contact-us" label="تواصل معنا" />
         </ul>
       </nav>
-      <div className='flex text-white mt-5'>
-        <RiLinkedinFill className='cursor-pointer mr-5 rounded-3xl border-[1px] border-white p-[6px] text-3xl hover:bg-blue-900 hover:border-blue-900 duration-300' />
-        <TiSocialFacebook className='cursor-pointer mr-5 rounded-3xl border-[1px] border-white p-[6px] text-3xl hover:bg-blue-500 hover:border-blue-500 duration-300' />
-        <FaTwitter className='cursor-pointer mr-5 rounded-3xl border-[1px] border-white p-[6px] text-3xl hover:bg-blue-400 hover:border-blue-400 duration-300' />
+      <div className='flex text-white mt-2 md:mt-5'>
+        <RiLinkedinFill className='cursor-pointer rounded-3xl border-[1px] border-white p-[6px] text-3xl hover:bg-blue-900 hover:border-blue-900 duration-300' />
+        <TiSocialFacebook className='cursor-pointer mx-5 rounded-3xl border-[1px] border-white p-[6px] text-3xl hover:bg-blue-500 hover:border-blue-500 duration-300' />
+        <FaTwitter className='cursor-pointer rounded-3xl border-[1px] border-white p-[6px] text-3xl hover:bg-blue-400 hover:border-blue-400 duration-300' />
       </div>
     </footer>
   );
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,6 +1,6 @@
 export default function Link({ toPlace, label, className }) {
   return(
-    <a href={toPlace} className={`pb-2 md:mb-0 md:ml-4 border-b-2 border-transparent hover:border-[#DD1015] duration-300 ${className}`}>
+    <a href={toPlace} className={`pb-2 md:mb-0 border-b-2 border-transparent hover:border-[#DD1015] duration-300 ${className}`}>
       {label}
     </a>
   );
