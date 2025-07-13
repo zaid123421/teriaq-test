21a35fd8daaab6b068d1c51d239d7e06a10440f5
Zaid Alshamaa <zshamaa20@gmail.com>
2025-07-13 13:41:42 +0300

    add z-index to the ul in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ export default function Header({ img, text, className }) {
       <nav className='flex items-center justify-between'>
         <img className="md:mr-[10px] hidden md:block" alt='Logo' src={img} />
         <div className="flex flex-row-reverse items-center justify-between">
-          <ul className={`md:flex flex-row-reverse text-center items-center ${text} ${active ? `py-5 rounded-2xl bg-gray-500 flex flex-col w-full absolute top-[100%] left-0` : `hidden`}`}>
+          <ul className={`z-[10] md:flex flex-row-reverse text-center items-center ${text} ${active ? `py-5 rounded-2xl bg-gray-500 flex flex-col w-full absolute top-[100%] left-0` : `hidden`}`}>
             <Link className="mb-2 md:ml-4" toPlace="/" label="الرئيسية" />
             <Link className="mb-2 md:ml-4" toPlace="#who-us" label="من نحن" />
             <Link className="mb-2 md:ml-4" toPlace="#most-popular" label="الأصناف الأكثر طلباً" />
